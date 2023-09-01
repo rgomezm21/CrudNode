d5424a78eefa8785d458b0a9c1743118427a4992
Raul Gomez <rauleduard132@gmail.com>
2023-09-01 15:55:58 -0500

    tercer commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,13 +5,7 @@ const { deleteUser } = require('../controller/deleteUser');
 const { getUserId } = require('../controller/getUserId');
 const { putUser } = require('../controller/putUser');
 
-
-
-
-
-
 const router = Router()
-
 router.get('/api/users', getUser);
 router.post('/api/users', validateUser, postUser);
 router.delete('/api/users/:idOrName', deleteUser)
