5964c1897389a0b34f012ef38aa7cff7a1d43300
Raul Gomez <rauleduard132@gmail.com>
2023-09-28 15:04:25 -0500

    numero float en validaciones y BD

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ function createTables() {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         producto TEXT,
         cliente TEXT,
-        cantidad INTEGER,
+        cantidad TEXT,
         valor_pagado INTEGER,
         fecha_compra DATE
 
