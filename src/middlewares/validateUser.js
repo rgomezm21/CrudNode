53a488e1e6f66b1f21b4056f1e100ea5d66fb5f9
Raul <rauleduard132@gmail.com>
2023-10-12 17:26:09 -0500

    modificacion validaciones

--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -23,9 +23,6 @@ const validate = validations => {
 const userValidation = validate([
   body('producto').isString().notEmpty().withMessage('Producto obligatorio'),
   body('cliente').isString().notEmpty().withMessage('Cliente obligatorio'),
-  body('cantidad')
-      .custom(value => /^\d+(\.\d{1,2})?$/.test(value))
-      .withMessage('Cantidad obligatoria (entero o decimal con hasta 2 decimales)'),
   body('valor_pagado').isInt().withMessage('Valor_pagado Obligatorio'),
   body('fecha_compra').isISO8601().withMessage('Fecha de compra obligatoria: AAAA/MM/DD'),
 ]);
